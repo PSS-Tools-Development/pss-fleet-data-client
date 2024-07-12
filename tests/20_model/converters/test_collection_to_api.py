a5eb1c120387ed4ba9b6fae471f6ca3b1b04c98e
The worst <5472896+Zukunftsmusik@users.noreply.github.com>
2024-07-12 15:39:33 +0200

    Delete unused test files

--- tests/20_model/converters/test_collection_to_api.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-
-import pytest
-from pssapi.entities import Alliance as PssAlliance
-from pssapi.entities import User as PssUser
-
-from src.client.model import AllianceHistory, Collection, CollectionMetadata, UserHistory
-from src.client.model.api import ApiAlliance, ApiAllianceHistory, ApiCollection, ApiCollectionMetadata, ApiUser, ApiUserHistory
-from src.client.model.converters import FromAPI
