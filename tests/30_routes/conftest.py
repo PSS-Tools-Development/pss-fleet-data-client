bf3a426d987ac30313a1b77a0f0d668fd6f961b3
The worst <5472896+Zukunftsmusik@users.noreply.github.com>
2024-07-15 10:54:07 +0200

    Fix scopes and add base_url fixture

--- tests/30_routes/conftest.py
+++ tests/30_routes/conftest.py
@@ -9,7 +9,7 @@ def response_get_collection_200(api_collection: ApiCollection):
     return api_collection.model_dump_json()
 
 
-@pytest.fixture(scope="session", autouse=True)
+@pytest.fixture(scope="function", autouse=True)
 def test_client(base_url):
     client = PssFleetDataClient(
         base_url=base_url,
@@ -21,6 +21,7 @@ def test_client(base_url):
 @pytest.fixture(scope="session")
 def base_url() -> str:
     return "https://fleetdata.dolores2.xyz"
+    # return "http://127.0.0.1:8000"
 
 
 @pytest.fixture(scope="function")
@@ -29,26 +30,26 @@ def get_collection_url(base_url) -> str:
 
 
 @pytest.fixture(scope="function")
-def mock_response_get_collection_200(api_collection: ApiCollection, httpx_mock):
+def mock_response_get_collection_200(api_collection: ApiCollection, base_url: str, httpx_mock):
     httpx_mock.add_response(
         method="GET",
-        url="https://fleetdata.dolores2.xyz/collections/1",
+        url=f"{base_url}/collections/1",
         text=api_collection.model_dump_json(),
     )
 
 
 @pytest.fixture(scope="function")
-def mock_response_get_collection_404(api_collection: ApiCollection, httpx_mock):
+def mock_response_get_collection_404(base_url: str, httpx_mock):
     httpx_mock.add_response(
         method="GET",
-        url="https://fleetdata.dolores2.xyz/collections/1",
+        url=f"{base_url}/collections/1",
         status_code=404,
         json={
             "code": "COLLECTION_NOT_FOUND",
             "message": "The requested Collection could not be found.",
             "details": "There is no Collection with the ID '1'.",
             "timestamp": "2024-07-13T14:04:30.696117+00:00",
-            "url": "http://fleetdata.dolores2.xyz/collections/1",
+            "url": f"{base_url}/collections/1",
             "suggestion": "Check the provided `collectionId` parameter in the path.",
             "links": [],
         },
@@ -56,17 +57,17 @@ def mock_response_get_collection_404(api_collection: ApiCollection, httpx_mock):
 
 
 @pytest.fixture(scope="function")
-def mock_response_get_collection_422(api_collection: ApiCollection, httpx_mock):
+def mock_response_get_collection_422(base_url: str, httpx_mock):
     httpx_mock.add_response(
         method="GET",
-        url="https://fleetdata.dolores2.xyz/collections/f",
+        url=f"{base_url}/collections/f",
         status_code=422,
         json={
             "code": "PARAMETER_COLLECTION_ID_INVALID",
             "message": "The provided value for the parameter `collectionId` is invalid.",
             "details": "Input should be a valid integer, unable to parse string as an integer",
             "timestamp": "2024-07-13T14:07:26.955843+00:00",
-            "url": "http://fleetdata.dolores2.xyz/collections/f",
+            "url": f"{base_url}/collections/f",
             "suggestion": "",
             "links": [],
         },
