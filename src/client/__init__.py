7bd18b17bac0538e3475d542b73c2b4ccb34eaab
The worst <5472896+Zukunftsmusik@users.noreply.github.com>
2024-07-15 10:54:22 +0200

    Add more stuff to package

--- src/client/__init__.py
+++ src/client/__init__.py
@@ -1,6 +1,16 @@
-from .model import PssFleetDataClient
+from pssapi.entities import Alliance as PssAlliance
+from pssapi.entities import User as PssUser
+
+from .model import Collection, CollectionMetadata, PssFleetDataClient, exceptions
 
 
 __all__ = [
+    # Modules
+    exceptions.__name__,
+    # Classes
+    Collection.__name__,
+    CollectionMetadata.__name__,
+    PssAlliance.__name__,
     PssFleetDataClient.__name__,
+    PssUser.__name__,
 ]
