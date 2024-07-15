4c0bf3d4d26f3788cdd65168155dc5fb3f168dff
The worst <5472896+Zukunftsmusik@users.noreply.github.com>
2024-07-15 15:22:59 +0200

    Add default for max_tournament_battle_attempts

--- src/client/model/models.py
+++ src/client/model/models.py
@@ -37,7 +37,7 @@ class CollectionMetadata(BaseModel):
     """Determines, whether a monthly fleet tournament was running at the time of recording the data in this Collection."""
     schema_version: int
     """The version of the schema of the data in this Collection."""
-    max_tournament_battle_attempts: Optional[int]
+    max_tournament_battle_attempts: Optional[int] = None
     """The maximum number of tournament battles any given player can do on a given monthly fleet tournament day."""
     data_version: Optional[int]
     """The schema version with which this data was first collected and stored."""
