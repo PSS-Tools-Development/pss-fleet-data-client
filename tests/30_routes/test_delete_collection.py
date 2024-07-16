73e96bd572a32a8fe92cba1942337ac83f203831
The worst <5472896+Zukunftsmusik@users.noreply.github.com>
2024-07-16 13:50:17 +0200

    Add tests for 401 & 403

--- tests/30_routes/test_delete_collection.py
+++ tests/30_routes/test_delete_collection.py
@@ -1,13 +1,35 @@
 import pytest
 
 from client import PssFleetDataClient
-from client.core.exceptions import CollectionNotDeletedError, CollectionNotFoundError, InvalidCollectionIdError
+from client.core.exceptions import (
+    CollectionNotDeletedError,
+    CollectionNotFoundError,
+    InvalidCollectionIdError,
+    MissingAccessError,
+    NotAuthenticatedError,
+)
 
 
 @pytest.mark.usefixtures("mock_response_empty_get_204")
 async def test_delete_collection_204(test_client: PssFleetDataClient):
     response = await test_client.delete_collection(1)
     assert response is True
+
+
+@pytest.mark.usefixtures("mock_response_401")
+async def test_delete_collection_401(
+    test_client: PssFleetDataClient,
+):
+    with pytest.raises(NotAuthenticatedError):
+        _ = await test_client.delete_collection(1)
+
+
+@pytest.mark.usefixtures("mock_response_403")
+async def test_delete_collection_403(
+    test_client: PssFleetDataClient,
+):
+    with pytest.raises(MissingAccessError):
+        _ = await test_client.delete_collection(1)
 
 
 @pytest.mark.usefixtures("mock_response_collection_not_found")
