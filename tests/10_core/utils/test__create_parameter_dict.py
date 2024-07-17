04e428e3ef962b333e962f0dcda73f105d8a1481
The worst <5472896+Zukunftsmusik@users.noreply.github.com>
2024-07-17 12:58:42 +0200

    Move test to correct module

--- tests/10_core/utils/test__create_parameter_dict.py
+++ tests/10_core/utils/test__create_parameter_dict.py
@@ -1,12 +1,12 @@
 from typing import Any, Optional
 
-import helper_test_cases
 import pytest
+import utils_test_cases
 
 from client.core import utils
 
 
-@pytest.mark.parametrize(["input", "expected_output"], helper_test_cases.parameter_dicts)
+@pytest.mark.parametrize(["input", "expected_output"], utils_test_cases.parameter_dicts)
 def test__create_parameter_dict(input: Optional[dict[str, Any]], expected_output: dict[str, Any]):
     if input is None:
         output = utils.create_parameter_dict()
