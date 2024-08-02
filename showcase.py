652fa368384e6c92e0af5be5e428f4dc0a5ee5b0
The worst <5472896+Zukunftsmusik@users.noreply.github.com>
2024-08-02 15:21:53 +0200

    Add comments to showcase

--- showcase.py
+++ showcase.py
@@ -4,14 +4,38 @@ from datetime import datetime, timezone
 from pss_fleet_data import ParameterInterval, PssFleetDataClient, utils
 
 
+# Create the client, specifying the API server's base URL. If you don't specify a base URL, it defaults to https://fleetdata.dolores2.xyz
 client = PssFleetDataClient(base_url="https://fleetdata.dolores2.xyz")
 
 
 async def print_latest_tournament_results():
-    most_recent_timestamp = utils.get_most_recent_timestamp(datetime.now(tz=timezone.utc), ParameterInterval.MONTHLY)  #
+    # Get the current time
+    now = datetime.now(tz=timezone.utc)
+
+    # Tournament results are collected shortly before the start of a new month.
+    # Calculate the most recent start of month relative to now.
+    most_recent_timestamp = utils.get_most_recent_timestamp(now, ParameterInterval.MONTHLY)
+
+    # Then get the most recent data collected before the calculated timestamp.
     collection = await client.get_most_recent_collection_by_timestamp(most_recent_timestamp)
+
+    # Work with the Collection.
     print(f"Collection with ID {collection.metadata.collection_id} collected at {collection.metadata.timestamp}.")
     print(f"It has collected {collection.metadata.fleet_count} fleets and {len(collection.users)} players.")
+
+    # You can use the Collection's ID in other methods, too, e.g. get the top 100 players at the end of the month.
+    top_100_users = await client.get_top_100_users_from_collection(collection.metadata.collection_id)
+    print(f"The player ranked 3rd last month was: {top_100_users[2].name}")
+
+    # Or obtain the player stats of the 2nd best player last month over time
+    user_history = await client.get_user_history(top_100_users[1].id, interval=ParameterInterval.MONTHLY)
+    print(
+        "Found history for player %s from %s to %s (%i)",
+        top_100_users[1].name,
+        user_history[0].collection.timestamp.isoformat(),
+        user_history[-1].collection.timestamp.isoformat(),
+        len(user_history),
+    )
 
 
 if __name__ == "__main__":
